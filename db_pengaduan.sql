e94dba88ae0ae147f0b00584098289905783c121
adekcantik <50389589+adekcantik@users.noreply.github.com>
2020-12-20 14:09:55 +0700

    update terakhir

--- db_pengaduan.sql
+++ db_pengaduan.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 16, 2020 at 08:24 AM
+-- Generation Time: Dec 20, 2020 at 08:09 AM
 -- Server version: 10.4.17-MariaDB
 -- PHP Version: 7.4.13
 
@@ -20,6 +20,54 @@ SET time_zone = "+00:00";
 --
 -- Database: `db_pengaduan`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `department`
+--
+
+CREATE TABLE `department` (
+  `id` int(11) NOT NULL,
+  `department` varchar(255) DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  `deleted_at` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC;
+
+--
+-- Dumping data for table `department`
+--
+
+INSERT INTO `department` (`id`, `department`, `created_at`, `updated_at`, `deleted_at`) VALUES
+(1, 'Dinas Pariwisata', NULL, NULL, NULL),
+(2, 'Dinas Kominfo', NULL, NULL, NULL),
+(3, 'Dinas Kesehatan', NULL, NULL, NULL),
+(4, 'Pemkot Tebing Tinggi', NULL, NULL, NULL),
+(5, 'Dinas Keamanan', NULL, NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `komentar`
+--
+
+CREATE TABLE `komentar` (
+  `id` int(11) NOT NULL,
+  `idpengaduan` int(255) DEFAULT NULL,
+  `komentar` varchar(255) DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  `deleted_at` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC;
+
+--
+-- Dumping data for table `komentar`
+--
+
+INSERT INTO `komentar` (`id`, `idpengaduan`, `komentar`, `created_at`, `updated_at`, `deleted_at`) VALUES
+(1, 2, 'adasdasd', '2020-12-19 22:32:10', '2020-12-19 22:32:10', NULL),
+(2, 1, 'adasdsadasd', '2020-12-19 22:45:25', '2020-12-19 22:45:25', NULL);
 
 -- --------------------------------------------------------
 
@@ -58,25 +106,27 @@ CREATE TABLE `pengaduan` (
   `phone` varchar(255) DEFAULT NULL,
   `department` varchar(255) DEFAULT NULL,
   `message` text DEFAULT NULL,
+  `status` varchar(255) DEFAULT NULL,
+  `done_at` datetime DEFAULT NULL,
   `created_at` datetime DEFAULT NULL,
-  `updated_at` datetime DEFAULT NULL
+  `updated_at` datetime DEFAULT NULL,
+  `deleted_at` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC;
 
 --
 -- Dumping data for table `pengaduan`
 --
 
-INSERT INTO `pengaduan` (`id`, `nik`, `nama`, `email`, `phone`, `department`, `message`, `created_at`, `updated_at`) VALUES
-(1, '123', '123', '123', '123', 'Dinas Pariwisata', 'TEST', NULL, NULL),
-(2, '456', '456', '456', '123', 'Dinas Pariwisata', 'TEST', NULL, NULL),
-(3, '123', 'asdad', 'asdasd@asdasd.com', '123', 'Dinas Pariwisata', 'TEST', '2020-12-11 09:56:12', '2020-12-11 09:56:12'),
-(4, '123', '123123', 'add@gmail.com', '123', 'Kominfo', 'TEST', '2020-12-11 10:00:21', '2020-12-11 10:00:21'),
-(5, 'adasdsa', 'asdasd', 'asdsad@gmail.com', '123', 'Kominfo', 'TEST', '2020-12-11 10:02:31', '2020-12-11 10:02:31'),
-(6, '12313123', '123123', '123123@gmail.com', '123', 'Pemerintahan Tebing Tinggi', 'TEST', '2020-12-11 10:04:41', '2020-12-11 10:04:41'),
-(7, 'asdasdsd', 'asdasd', 'asdsad@asdsad.com', '123', 'Kantor Perpajakan', 'TEST', '2020-12-11 21:11:50', '2020-12-11 21:11:50'),
-(8, '123123', '123123', 'a@a.com', '1231323', 'Pemerintahan Tebing Tinggi', 'adadasd', '2020-12-15 23:15:05', '2020-12-15 23:15:05'),
-(9, '123123', '123123@adsa.com', 'asdasd@asdsd.com', '123123123', 'Dinas Pariwisata', 'adasd', '2020-12-15 23:16:28', '2020-12-15 23:16:28'),
-(10, '12345678', 'adasdasdsad', 'kominfo@gmail.com', '081398000964', 'Kominfo', 'asdasdasdsad', '2020-12-16 01:18:52', '2020-12-16 01:18:52');
+INSERT INTO `pengaduan` (`id`, `nik`, `nama`, `email`, `phone`, `department`, `message`, `status`, `done_at`, `created_at`, `updated_at`, `deleted_at`) VALUES
+(1, '123', '123', '123', '123', 'Dinas Pariwisata', 'TEST', 'Selesai', '2020-12-19 22:45:25', '2020-12-11 09:56:12', '2020-12-19 22:45:25', NULL),
+(2, '456', '456', '456', '123', NULL, 'TEST', 'Selesai', '2020-12-19 22:32:10', '2020-12-11 09:56:12', '2020-12-19 22:32:10', NULL),
+(3, '123', 'asdad', 'asdasd@asdasd.com', '123', 'Dinas Pariwisata', 'TEST', 'Selesai', '2020-12-19 22:32:10', '2020-12-11 09:56:12', NULL, NULL),
+(4, '123', '123123', 'add@gmail.com', '123', 'Dinas Pariwisata', 'TEST', 'Diproses', NULL, '2020-12-11 10:00:21', '2020-10-19 22:50:26', NULL),
+(5, 'adasdsa', 'asdasd', 'asdsad@gmail.com', '123', 'Pemkot Tebing Tinggi', 'TEST', 'Diproses', NULL, '2020-12-11 10:02:31', '2020-12-19 23:46:54', NULL),
+(6, '12313123', '123123', '123123@gmail.com', '123', NULL, 'TEST', 'Diterima', NULL, '2020-12-11 10:04:41', NULL, NULL),
+(7, 'asdasdsd', 'asdasd', 'asdsad@asdsad.com', '123', NULL, 'TEST', 'Diterima', NULL, '2020-12-11 21:11:50', NULL, NULL),
+(8, '123123', '123123', 'a@a.com', '1231323', NULL, 'adadasd', 'Diterima', NULL, '2020-12-15 23:15:05', NULL, NULL),
+(9, '123123', '123123@adsa.com', 'asdasd@asdsd.com', '123123123', NULL, 'adasd', 'Diterima', NULL, '2020-12-15 23:16:28', NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -114,24 +164,37 @@ CREATE TABLE `users` (
   `department` varchar(255) DEFAULT NULL,
   `isadmin` tinyint(1) DEFAULT 0,
   `created_at` datetime DEFAULT NULL,
-  `updated_at` datetime DEFAULT NULL
+  `updated_at` datetime DEFAULT NULL,
+  `deleted_at` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `nik`, `passwd`, `department`, `isadmin`, `created_at`, `updated_at`) VALUES
-(1, '123', '123', 'Dinas Pariwisata', 0, NULL, NULL),
-(2, '111', '111', 'Dinas Perhubungan', 0, NULL, NULL),
-(3, '222', '222', 'Kominfo', 0, NULL, NULL),
-(4, '333', '333', 'Pemerintahan Tebing Tinggi', 0, NULL, NULL),
-(5, '444', '444', 'Kantor Perpajakan', 0, NULL, NULL),
-(6, 'vero', 'vero', NULL, 1, NULL, NULL);
+INSERT INTO `users` (`id`, `nik`, `passwd`, `department`, `isadmin`, `created_at`, `updated_at`, `deleted_at`) VALUES
+(1, '123@123.com', '123', 'Dinas Pariwisata', 0, NULL, NULL, NULL),
+(2, '111', '111', 'Dinas Perhubungan', 0, NULL, NULL, NULL),
+(3, '222', '222', 'Kominfo', 0, NULL, NULL, NULL),
+(4, '333', '333', 'Pemerintahan Tebing Tinggi', 0, NULL, NULL, NULL),
+(5, '444', '444', 'Kantor Perpajakan', 0, NULL, NULL, NULL),
+(6, 'vero@gmail.com', 'vero', NULL, 1, NULL, NULL, NULL);
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `department`
+--
+ALTER TABLE `department`
+  ADD PRIMARY KEY (`id`) USING BTREE;
+
+--
+-- Indexes for table `komentar`
+--
+ALTER TABLE `komentar`
+  ADD PRIMARY KEY (`id`) USING BTREE;
 
 --
 -- Indexes for table `migrations`
@@ -162,6 +225,18 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `department`
+--
+ALTER TABLE `department`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `komentar`
+--
+ALTER TABLE `komentar`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
 -- AUTO_INCREMENT for table `migrations`
 --
 ALTER TABLE `migrations`
@@ -171,7 +246,7 @@ ALTER TABLE `migrations`
 -- AUTO_INCREMENT for table `pengaduan`
 --
 ALTER TABLE `pengaduan`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `user`
